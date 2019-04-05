237840a1071baaf0ba9092cd9a7729e2e8d93d2b
Fredrik Sundqvist <fredrik.sundqvist@afconsult.com>
2019-04-05 08:52:55 +0200

    Update story for Button

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 import Button from './Button';
 
-storiesOf('Components|Button', module).add('Examples', () => (
-  <Button color="primary">primary</Button>
-));
+storiesOf('Components|Button', module)
+  .addDecorator(withInfo)
+  .add('Examples', () => <Button color="primary">primary</Button>, {
+    info: {
+      inline: true,
+      text:
+        'The component also support [HTML DOM Input Button](https://www.w3schools.com/jsref/dom_obj_button.asp) Object Properties.',
+    },
+  });
